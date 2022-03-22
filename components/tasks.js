1bd3fe6f1e5858cbac032d2677b6f1a63bfa65ae
medobarakat <ahmedbarakat2401@gmail.com>
2022-03-22 13:22:03 +0200

    editing task card

--- components/tasks.js
+++ components/tasks.js
@@ -4,15 +4,50 @@ import React from "react";
 const Tasks = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.task}>
+      <View style={styles.left}>
         <View style={styles.rectangle}></View>
         <Text style={styles.text}>Likes </Text>
-        <View style={styles.circle}></View>
       </View>
+      <View style={styles.circle}></View>
     </View>
   );
 };
 
 export default Tasks;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 20,
+    padding: 15,
+    marginHorizontal: 15,
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  left: {
+    flexDirection: "row",
+    alignItems: "center",
+    flexWrap: "wrap",
+  },
+  rectangle: {
+    width: 24,
+    height: 24,
+    backgroundColor: "#55BCF6",
+    opacity: 0.4,
+    borderRadius: 5,
+    marginRight: 15,
+  },
+  text: {
+    fontSize: 14,
+    maxWidth: "80%",
+  },
+  circle: {
+    width: 12,
+    height: 12,
+    borderColor: "#55BCF6",
+    borderRadius: 5,
+    borderWidth: 2,
+  },
+});
